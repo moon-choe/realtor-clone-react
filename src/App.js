2d7ed8a8a514cd90c95ead7e8c8aa82c2edbe2ae
Moon Choe <m970681@mac21057.int.8451.com>
2026-01-20 10:41:40 -0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+  
 function App() {
   return (
-   <>
-    <h1>
-      hello world
-    </h1>
-   </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
+      </Routes>
+    </Router>
   );
 }
 
